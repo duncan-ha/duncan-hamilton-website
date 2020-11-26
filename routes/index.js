476ab7baf308133788e89ac23cd968a894e74084
duncan-ha <duncan.c.hamilton@gmail.com>
2020-11-26 01:54:31 -0500

    init

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const app = require('express').Router();
+module.exports = app;
+
+app.get('/', (req, res, next) => {
+  res.render('index', { title: 'Home' });
+});
+
+app.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/test', (req, res, next) => {
+  res.render('test', { title: 'Success' });
+});
+
